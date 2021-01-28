7a37446b84b1778733c362a65a8b7159f0643c6d
Moonbbyte <63923585+Moonbbyte@users.noreply.github.com>
2021-01-28 07:29:47 -0600

    ejercicio1

---
+++ Ejercicio_1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+//Referenciacion: Es la obtencion de la direccion de una variable
+int main() {
+    int dir_var=15;
+    printf("La dirección de dir_var es: %p \n", &dir_var);
+    return 0; 
+}
+  
